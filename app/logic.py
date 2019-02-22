03b48a0a4a6276fa0320a9a64ca8382ebb2d5a8d
Oliver Tonnesen <oliver@tonnesen.com>
2019-02-22 11:53:53 -0800

    Added floodfill to logic

--- app/logic.py
+++ app/logic.py
@@ -15,6 +15,17 @@ class point:
     def right(self):
         return point(self.x+1, self.y)
 
+    def direction_str(self, p):
+        dx = p.x - self.x
+        dy = p.y - self.y
+        if abs(dy) > abs(dx):
+            if dy < 0:
+                return 'up'
+            return 'down'
+        if dx < 0:
+            return 'left'
+        return 'right'
+
     def neighbors(self):
         return [self.up(), self.down(), self.left(), self.right()]
 
@@ -40,15 +51,19 @@ class point:
     def __repr__(self):
         return 'point(x:{},y:{})'.format(self.x, self.y)
 
+def parse_point(point_json):
+    return point(point_json['x'], point_json['y'])
+
 
 def logic(data):
     '''
     Get current time
     while time_elapsed < 190ms
        do minimax
-    return value
+    return highest value move
     '''
-    return 'up'
+    moves = {p:floodfill(data, p) for p in parse_point(data['you']['body'][0]).neighbors()}
+    return parse_point(data['you']['body'][0]).direction_str(max(moves, key=moves.get))
 
 def get_board_value(data):
     '''
@@ -115,7 +130,24 @@ def main():
                            'health': 91,
                            'id': 'af1b9cb9-3c7a-44c0-8ef7-9850e19ca0af',
                            'name': 't1'}}
+    data2 = {   'board': {   'food': [{'x': 8, 'y': 1}, {'x': 6, 'y': 8}],
+                         'height': 11,
+                         'snakes': [   {   'body': [   {'x': 8, 'y': 8},
+                                                       {'x': 9, 'y': 8},
+                                                       {'x': 9, 'y': 9}],
+                                           'health': 98,
+                                           'id': 'gs_j8j6TVk7ykQfMfv7mQqd8kWP',
+                                           'name': 'otonnesen/Test Snake Please '
+                                                   'Ignore'}],
+                         'width': 11},
+            'game': {'id': '820c1d82-cd8c-414d-846b-34dfb6bfa93e'},
+            'turn': 2,
+            'you': {   'body': [{'x': 1, 'y': -1}, {'x': 1, 'y': 0}, {'x': 1, 'y': 1}],
+                       'health': 98,
+                       'id': 'gs_7X6KXcCRwWc9qkFPGxTr6HYX',
+                       'name': 'otonnesen/test snake 2'}}
     print(floodfill(data, point(data['you']['body'][0]['x']+1, data['you']['body'][0]['y'])))
+    print(logic(data))
 
 if __name__ == '__main__':
     main()
